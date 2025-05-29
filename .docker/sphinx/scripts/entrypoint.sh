fe87b6bde154d57892e1ec8ba196cf806e5fcbb3
Vitor Mattos <vitor@php.rio>
2025-05-29 14:48:56 -0300

    chore: move all files to root folder
    Following the same pattern of Nextcloud documentation, I moved all files
    of docs folder to root folder.
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .docker/sphinx/scripts/entrypoint.sh
+++ .docker/sphinx/scripts/entrypoint.sh
@@ -8,14 +8,14 @@ echo "🔍 Checking for required dependencies..."
 echo "⬆️ Upgrading pip..."
 python -m pip install --upgrade pip
 
-echo "📥 Installing packages from /app/docs/requirements.txt..."
-pip install -r "/app/docs/requirements.txt"
+echo "📥 Installing packages from /app/requirements.txt..."
+pip install -r "/app/requirements.txt"
 
 echo "✅ Dependencies installed installed."
 
 echo "🛠️ Building documentation..."
 
-sphinx-autobuild --port 0 /app/docs/user_manual /app/_build/user_manual &
-sphinx-autobuild --port 0 /app/docs/admin_manual /app/_build/admin_manual &
-sphinx-autobuild --port 0 /app/docs/developer_manual /app/_build/developer_manual &
-sphinx-autobuild --port 0 /app/docs/main /app/_build
+sphinx-autobuild --port 0 /app/user_manual /app/_build/user_manual &
+sphinx-autobuild --port 0 /app/admin_manual /app/_build/admin_manual &
+sphinx-autobuild --port 0 /app/developer_manual /app/_build/developer_manual &
+sphinx-autobuild --port 0 /app/main /app/_build
