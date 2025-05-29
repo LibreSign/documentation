0889f6d4fbbc777cd22ffb2c4d0d1921760eb1a9
Crisciany Souza <crisciany.souza@librecode.coop>
2025-05-29 14:55:56 -0400

    add logo libresign
    Signed-off-by: Crisciany Souza <crisciany.souza@librecode.coop>

--- main/conf.py
+++ main/conf.py
@@ -21,6 +21,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = "images/logo.png"
 html_theme_options = {
     'style_nav_header_background': '#184c4e',
 	'logo_only': True,
@@ -28,3 +29,4 @@ html_theme_options = {
 	'style_external_links': True,
 	'version_selector': False,
 }
+
