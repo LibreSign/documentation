cf2e1b161596a120305c264b73353f8159a52d9e
Vitor Mattos <vitor@php.rio>
2025-05-29 11:24:12 -0300

    chore: add dark theme button
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- docs/developer_manual/conf.py
+++ docs/developer_manual/conf.py
@@ -7,13 +7,19 @@ author = 'LibreSign Team'
 release = '1.0'
 
 extensions = [
-    'sphinxcontrib.openapi',
-    'sphinxcontrib.redoc',
+    'sphinx_rtd_theme',
+    'sphinx_rtd_dark_mode',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+	'logo_only': True,
+	'navigation_with_keys': True,
+	'style_external_links': True,
+	'version_selector': False,
+}
 html_static_path = ['_static']
 html_extra_path = ['html']
