6609c480ea1527d0c53b4173846ef6d9b7642c88
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-01 13:02:07 -0300

    fix: 404
    At last commit I added the conf to implement 404 page, but the main
    page, that need to have the 404 page, wasn't loading the conf.py from
    root folder.
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- main/conf.py
+++ main/conf.py
@@ -1,32 +1,5 @@
 import os
 import sys
 import datetime
-
-now = datetime.datetime.now()
-
-copyright = str(now.year) + ' LibreCode coop'
-project = 'LibreSign Documentation'
-author = 'LibreSign Team'
-release = '1.0'
-
-
-release = '1.0'
-
-extensions = [
-    'sphinx_rtd_theme',
-    'sphinx_rtd_dark_mode',
-]
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-html_theme = 'sphinx_rtd_theme'
-html_logo = "images/logo.png"
-html_theme_options = {
-    'style_nav_header_background': '#184c4e',
-	'logo_only': True,
-	'navigation_with_keys': True,
-	'style_external_links': True,
-	'version_selector': False,
-}
-
+sys.path.insert(0, os.path.abspath('../'))
+from conf import *
